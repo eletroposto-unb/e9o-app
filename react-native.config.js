aec544700bb563cfdb856c2176f32b4ba1b868c6
Gabriela <gpivetta99@gmail.com>
2023-05-10 18:55:07 -0300

    Configuração inicial firebase para android

---
+++ react-native.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    dependencies: {
+      '@react-native-firebase/app': {
+        platforms: {
+          android: null, // desativar a vinculação automática do Android
+        },
+      },
+    },
+  };
+  
