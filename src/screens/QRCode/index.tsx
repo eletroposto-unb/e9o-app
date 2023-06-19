7d48d446ac1d86e57957f0f13143ecafe2f1b550
Kevin Luis <k.luis360@gmail.com>
2023-06-18 21:55:16 -0300

    feat: adding qr code screen

--- src/screens/QRCode/index.tsx
+++ src/screens/QRCode/index.tsx
@@ -1,12 +1,69 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+// import {useState, useEffect} from 'react';
+// import {
+//   StyleSheet,
+//   Text,
+//   View,
+//   TouchableOpacity,
+//   Image,
+//   Linking,
+// } from 'react-native';
+// import {Camera, useCameraDevices} from 'react-native-vision-camera';
+import React, {useState, useEffect} from 'react';
+import {Text, View, StyleSheet, Button} from 'react-native';
+import {BarCodeScanner} from 'expo-barcode-scanner';
 
-const QrCodeReader = () => {
+const QrCodeReader = (): JSX.Element => {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+
+  useEffect(() => {
+    const getBarCodeScannerPermissions = async () => {
+      const {status} = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    };
+
+    getBarCodeScannerPermissions();
+  }, []);
+
+  const handleBarCodeScanned = ({type, data}) => {
+    setScanned(true);
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
   return (
-    <View>
-      <Text>QrCodeReader</Text>
+    <View style={styles.container}>
+      <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        style={StyleSheet.absoluteFillObject}
+      />
+      {scanned && (
+        <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />
+      )}
     </View>
   );
 };
 
 export default QrCodeReader;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    marginTop: 15,
+    backgroundColor: 'white',
+  },
+  textError: {
+    color: 'red',
+  },
+});
