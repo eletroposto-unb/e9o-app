35081f896d08c016b714d806846fe92505d1dec3
Victor Samuel dos Santos Lucas <victor.samuelsantoss@gmail.com>
2023-07-06 16:31:57 -0300

    feat: adiciona loading

--- src/screens/History/index.tsx
+++ src/screens/History/index.tsx
@@ -1,15 +1,15 @@
 import React, {useState, useEffect, useContext} from 'react';
-import {View, Text, ScrollView, Button, StyleSheet} from 'react-native';
+import {View, Text, ScrollView, StyleSheet, RefreshControl} from 'react-native';
+import {Center} from 'native-base';
 import {Fonts} from '../../styles/fonts';
 import {FlexDiv} from '../../components/DisplayFlex/FlexDiv';
-import {RowItem} from '../../components/RowItem';
 import {NativeBaseProvider} from 'native-base';
-import {Image, Svg} from 'react-native-svg';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import coin from '../../assets/coin.png';
-import {SECUNDARY, WHITE} from '../../styles/colors';
+import {SECUNDARY, WHITE, BACKGROUND} from '../../styles/colors';
 import Details from './Details';
 import dayjs from 'dayjs';
+import SpinnerLoading from '../../components/SpinnerLoading';
+import NoHistoryMessage from './NoHistoryMessage';
 import 'dayjs/locale/pt-br';
 dayjs.locale('pt-br');
 
@@ -21,6 +21,7 @@ const History = () => {
   const {user} = useContext(AuthContext);
   const [histories, setHistories] = useState<History[]>([]);
   const [showDetails, setShowDetails] = useState<any>(null);
+  const [loading, setLoading] = useState(false);
 
   const handleShowDetails = detail => {
     setShowDetails(detail);
@@ -31,9 +32,11 @@ const History = () => {
   }, []);
 
   const handleHistory = async () => {
+    setLoading(true);
     const res = await getHistoryByCpf(user.cpf);
     console.log('first', res.value?.history.length);
     setHistories(res.value?.history);
+    setLoading(false);
   };
 
   const handleFormatDate = (date: Date) => {
@@ -47,7 +50,16 @@ const History = () => {
 
   return (
     <NativeBaseProvider>
-      <ScrollView contentContainerStyle={{flexGrow: 1}}>
+      <ScrollView
+        contentContainerStyle={{flexGrow: 1}}
+        refreshControl={
+          <RefreshControl
+            refreshing={loading}
+            onRefresh={handleHistory}
+            colors={[BACKGROUND]}
+            tintColor={BACKGROUND}
+          />
+        }>
         <View
           style={{
             width: '100%',
@@ -62,7 +74,8 @@ const History = () => {
               <Text style={{...Fonts.title, textAlign: 'center'}}>
                 Histórico de uso
               </Text>
-              {histories.length >= 1 &&
+              {loading && <SpinnerLoading />}
+              {histories.length >= 1 ? (
                 histories.map((history, index) => {
                   return (
                     <TouchableOpacity
@@ -87,7 +100,12 @@ const History = () => {
                       </FlexDiv>
                     </TouchableOpacity>
                   );
-                })}
+                })
+              ) : (
+                <Center>
+                  <NoHistoryMessage />
+                </Center>
+              )}
             </>
           ) : (
             <Details handleBack={handleBack} history={showDetails} />
@@ -117,74 +135,3 @@ const styles = StyleSheet.create({
 });
 
 export default History;
-
-interface DetalhesProps {
-  handleBack: () => void;
-  data: any;
-}
-
-const Detalhes = ({handleBack, data}: DetalhesProps) => {
-  return (
-    <>
-      <Text style={Fonts.title}>Detalhes de uso</Text>
-      <FlexDiv direction="column" gap={10}>
-        <RowItem>
-          <FlexDiv direction="column" gap={10}>
-            <FlexDiv direction="column" gap={5}>
-              <Text style={{...Fonts.labelBlue, fontSize: 24}}>Local</Text>
-              <Text
-                style={{...Fonts.thinBlack, fontSize: 20, color: '#4F4F4F'}}>
-                {data.posto.endereco}
-              </Text>
-            </FlexDiv>
-            <Divider />
-            <FlexDiv direction="column" gap={5}>
-              <Text style={{...Fonts.labelBlue, fontSize: 24}}>Horario</Text>
-              <Text
-                style={{...Fonts.thinBlack, fontSize: 20, color: '#4F4F4F'}}>
-                {data.horarioEntrada}
-              </Text>
-            </FlexDiv>
-            <Divider />
-            <FlexDiv direction="column" gap={5}>
-              <Text style={{...Fonts.labelBlue, fontSize: 24}}>
-                Tempo de uso
-              </Text>
-              <Text
-                style={{...Fonts.thinBlack, fontSize: 20, color: '#4F4F4F'}}>
-                {dayjs(data.horarioSaida).diff(dayjs(data.horarioEntrada), 'm')}
-              </Text>
-            </FlexDiv>
-            <Divider />
-            <FlexDiv direction="column" gap={5}>
-              <Text style={{...Fonts.labelBlue, fontSize: 24}}>Preço</Text>
-              <FlexDiv direction="row" gap={10}>
-                <Text
-                  style={{...Fonts.thinBlack, fontSize: 20, color: '#4F4F4F'}}>
-                  {data.valorTotal.toString() + ' moedas'}
-                </Text>
-                <Svg width="24" height="24" viewBox="0 0 24 24" fill="white">
-                  <Image width={24} height={24} href={coin} />
-                </Svg>
-              </FlexDiv>
-            </FlexDiv>
-          </FlexDiv>
-        </RowItem>
-        <Button onPress={handleBack} title="Voltar" />
-      </FlexDiv>
-    </>
-  );
-};
-
-const Divider = () => {
-  return (
-    <View
-      style={{
-        width: '100%',
-        height: 1,
-        backgroundColor: '#BDBDBD',
-        marginVertical: 10,
-      }}
-    />
-  );
-};
