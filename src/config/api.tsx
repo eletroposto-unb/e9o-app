03ad2d70fe76ca20217ab0eb95bc76a7951781df
Victor Samuel dos Santos Lucas <victor.samuelsantoss@gmail.com>
2023-07-06 21:29:08 -0300

    styles: update styles

--- src/config/api.tsx
+++ src/config/api.tsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: 'https://api.eletrogama.online', // Defina a URL base da sua API
-  baseURL: 'http://192.168.1.5:8080',
+  baseURL: 'https://api.eletrogama.online', // Defina a URL base da sua API
+  // baseURL: 'http://192.168.1.5:8080',
   timeout: 10000, // Tempo máximo de espera por uma resposta (opcional)
 });
 
